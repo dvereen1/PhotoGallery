377c7776af9f7e1c2e63df4060d6bce607381141
Darian Vereen <vereen.darian@gmail.com>
2021-04-09 21:23:01 -0400

    Add base url to php file(s). Update URI paths of all links.

--- photoGallery.php
+++ photoGallery.php
@@ -3,6 +3,7 @@
     include_once("headNoNav.php");
 ?>
         <title>DJV Photo Gallery</title>
+        <base href = "/portfolio/photoGallery/">
         <link rel = "stylesheet" href = "/CSS/allProjectsModal.css">
         <link rel = "stylesheet"  type = "text/css" href = "photoGallery.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
